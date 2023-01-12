cd9c52678a2d71017aa780c24784d0fe10980a54
Jesper Andersson <h21jespa@du.se>
2023-01-12 22:43:47 +0100

    Test

--- 02-todo/client/Api.js
+++ 02-todo/client/Api.js
@@ -1,32 +1,11 @@
-/* I denna fil finns en klass för att hantera API-förfrågningar mot server (servern är det backend som skapades i lektion 5).
 
-Om ni vill starta precis denna kod måste ni först installera om node-paket och starta upp servern. Servern, såsom den ser ut i slutet av Lektion 6, finns i denna samma zip-fil. Om ni skulle köra denna kod mot backend såsom det såg ut efter Lektion 5, skulle det inte fungera, eftersom detta är koden såsom den ser ut efter Lektion 6 och några små förändringar gjordes även i servern under Lektion 6. 
-
-Gör då följande här i VS Code: 
-1. Öppna en terminal
-2. Skriv "cd 02-todo/server" (utan citattecken) och sedan enter
-3. Skriv "npm install" (utan citattecken) och sedan enter
-3. Skriv "node app.js" (utan citattecken) och enter. 
-
-Om servern startats korrekt syns nu texten "Server running on http://localhost:5000".
-
-*/
-
-/* För att skapa en klass används nyckelordet class följt av klassens namn. Klasser bör ha stor inledande bokstav och döpas enligt det som kallas PascalCase. Inga parenteser används vid skapande av en klass. */
 class Api {
-  /* Medlemsvariabel url, för att lagra en grund-url till servern. */
   url = '';
 
-  /* När en instans av klassen skapas skickas url in som parametern */
   constructor(url) {
-    /* Medlemsvariabeln url sätts till den sträng som man skickar in när man skapar en instans av klassen (det görs i script.js). Detta upplägg är för att göra denna klass generell. Tanken är att det ska gå att använda vår api-klass till olika HTTP-anrop, inte bara sådana för våran todo-applikation.   */
     this.url = url;
   }
 
-  /* Metod för att hatera att skapa resurser (Create i CRUD). Motsvarar ett anrop med metoden POST.   
-  
-  Create = POST
-  */
   create(data) {
     const JSONData = JSON.stringify(data);
     console.log(`Sending ${JSONData} to ${this.url}`);
@@ -48,9 +27,8 @@ class Api {
     );
   }
 
-  /* Read - GET */
+  
   getAll() {
-    /* I detta fetch-anrop behövs inga särskilda inställningar. Fetch kan ta bara url:en som parameter också, istället för att man skapar ett helt request-objekt och skickar in det. */
     return fetch(this.url)
       .then((result) => result.json())
       .then((data) => data)
@@ -58,23 +36,11 @@ class Api {
   }
 
   remove(id) {
-    /*  Innan ni går vidare med remove, så måste ni se till att server-koden från L5 har en sak från L5 fixad: 
-      I server/app.js ska  
-        res.header('Access-Control-Allow-Method', '*') 
-      ha ändrats till till 
-        res.header('Access-Control-Allow-Methods', '*'); (Method->Methods, alltså)  */
+   
 
-    /* Log för att se att rätt uppgift är på väg att tas bort */
-    console.log(`Removing task with id ${id}`);
+    console.log(`Removing movie with id ${id}`);
 
-    /* Här behövs, precis som vid POST, lite mer inställningar. Fetch behöver dock inte heller här ett requestobjekt. Det går bra att skicka de sakerna som man skulle ha skickat till requestobjektets konstruktor direkt till fetch-funktionen. 
-
-    Det som skickas in som förfrågan är alltså url, som första argument och en uppsättning inställningar i ett objekt, som andra argument. Precis som när POST-requesten skapades ovan, i create ovan. 
-
-    Det enda som finns i objektet, som skickas in som andra argument till fetch, är att sätta method till delete, eftersom det är den HTTP-metoden som ska användas här. 
-
-    Egentligen skulle jag ha kunnat satt exakt samma kedja av then-anrop här som vid create (POST) och getAll (READ), men det är inte helt relevant vad som kommer till baka från ett delete-anrop. 
-    */
+    
     return fetch(`${this.url}/${id}`, {
       method: 'DELETE'
     })
@@ -83,7 +49,7 @@ class Api {
   }
 
   update(data){
-    console.log(`Updating task with id ${data.id}`);
+    console.log(`Updating movie with id ${data.id}`);
 
     const JSONData = JSON.stringify(data);
     const request = new Request(this.url, {
@@ -103,41 +69,4 @@ class Api {
       .then((result) => result)
       .catch((err) => console.log(err));
   }
-  /***********************Labb 2 ***********************/
-  /* Här skulle det vara lämpligt att skriva en metod likt getAll,                        fetch(url, {options}) 
-  create och delete anropas från script.js när någon har markerat en uppgift som färdig. 
-  Denna metod bör ansvara för att göra en PUT eller PATCH-förfrågan till vårt backend, precis som create-metoden ansvarar för att göra ett POST-anrop.
-   Metoden här ska alltså motsvara Update = PUT/PATCH. En sådan förfrågan görs med hjälp av fetch(). 
-  
-  Beroende på om ni gör frontend eller backend först i labben behöver ni på något av ställena bestämma er för en av metoderna PUT
-   eller PATCH för denna förfrågan. (Du får välja själv, läs på om vad som verkar mest vettigt för din lösning). 
-   Använder du metoden PATCH här behöver i alla fall det vara patch som tas emot i servern också, app.patch(...), och vice versa om du väljer PUT. 
-  */
-
-  /*   
-  För att utföra en förfrågan med hjälp av fetch() behöver servern veta några saker om förfrågan (request). 
-  Först och främst behövs en url dit förfrågan ska skickas, 
-  sedan behövs också ett objekt med inställningar och detaljer om förfrågan,
-   detta objekt kallas vidare "{options}". Url och {options} kan sättas antingen i ett requestobjekts konstruktor; new Request(url, 
-    {options}), såsom det görs i create-metoden. Eller så skulle man kunna ange allt som annars skulle ha skickats till Request-objektets 
-    konstruktor inom parenteserna hos fetch() istället; fetch(url, {options})
-  
-  Här finns mer info om fetch-metoden: 
-  https://developer.mozilla.org/en-US/docs/Web/API/fetch.
-  */
-
-  /* Precis som vid create behöver även här {options} innehålla egenskapen body - dvs. de data som ska skickas till server. */
-
-  /* Det finns några sätt att utforma det som ska skickas i förfrågans body. Oavsett vad vi väljer ska det först översättas till JSON. 
-  
-  Alternativ 1: body består av ett helt task-objekt, som också inkluderar förändringen. Exempel: 
-  {id: 1,  title: "x", description: "x", dueDate: "x", completed: true/false}
-  Alternativ 2: request-objektets body består bara av förändringarna och vilken uppgift som ska förändras. Exempel:
-   {id: 1, completed: true/false }
-  
-  Om du hittar något annat sätt som funkar för dig, använd för all del det, så länge det uppnår samma sak. :)
-  
-  */
-
-  /***********************Labb 2 ***********************/
 }
--- 02-todo/client/script.js
+++ 02-todo/client/script.js
@@ -47,7 +47,7 @@ function validateField(field) {
     case "dueDate": {
       if (value.length === 0) {
         dueDateValid = false;
-        validationMessage = "Fältet 'Slutförd senast' är obligatorisk.";
+        validationMessage = "Fältet 'Producerad' är obligatorisk.";
       } else {
         dueDateValid = true;
       }
@@ -131,11 +131,7 @@ function renderTask(task) {
   html += `
     </li>`;
 
-  /***********************Labb 2 ***********************/
-  /* I ovanstående template-sträng skulle det vara lämpligt att sätta en checkbox, eller ett annat element som någon kan klicka på för att markera en uppgift som färdig. 
-  Det elementet bör, likt knappen för delete, också lyssna efter ett event (om du använder en checkbox, kolla på exempelvis w3schools vilket element som triggas hos en checkbox när dess värde förändras.). Skapa en eventlyssnare till det event du finner lämpligt. 
-  Funktionen behöver nog ta emot ett id, så den vet vilken uppgift som ska markeras som färdig. Det skulle kunna vara ett checkbox-element som har attributet on[event]="updateTask(id)". */
-  /***********************Labb 2 ***********************/
+
 
   return html;
 }
@@ -143,7 +139,7 @@ function renderTask(task) {
 function checkboxstate(checkbox, task) {
   var taskItem = document.getElementById(`task-${task.id}`);
   if (checkbox.checked) {
-    taskItem.style.backgroundColor = "Green";
+    taskItem.style.backgroundColor = "green";
   } else {
     taskItem.style.backgroundColor = "Inherit";
   }
@@ -161,27 +157,4 @@ function updateTask(data) {
   api.update(data).then((result) => {});
 }
 
-/***********************Labb 2 ***********************/
-/* Här skulle det vara lämpligt att skriva den funktion som angivits som eventlyssnare för när någon markerar en uppgift som färdig. Jag pratar alltså om den eventlyssnare som angavs i templatesträngen i renderTask. Det kan t.ex. heta updateTask. 
-  
-Funktionen bör ta emot ett id som skickas från <li>-elementet.
-*/
-
-/* Inuti funktionen kan ett objekt skickas till api-metoden update. Objektet ska som minst 
-innehålla id på den uppgift som ska förändras, samt egenskapen completed som true eller false, 
-beroende på om uppgiften markerades som färdig eller ofärdig i gränssnittet. 
-Det finns några sätt att utforma det som ska skickas till api.update-metoden. 
-Alternativ 1: objektet består av ett helt task-objekt, som också inkluderar förändringen. 
-Exempel: {id: 1,  title: "x", description: "x", dueDate: "x", completed: true/false}
-Alternativ 2: objektet består bara av förändringarna och id på den uppgift som ska förändras. Exempel: {id: 1, completed: true/false } 
-Om du hittar något annat sätt som funkar för dig, använd för all del det, så länge det uppnår samma sak. :)
-*/
-
-/* Anropet till api.update ska följas av then(). then() behöver, som bör vara bekant vid det här laget, 
-en callbackfunktion som ska hantera det som kommer tillbaka från servern via vår api-klass. 
-Inuti den funktionen bör listan med uppgifter renderas på nytt, så att den nyligen gjorda förändringen syns. */
-
-/***********************Labb 2 ***********************/
-
-/* Slutligen. renderList anropas också direkt, så att listan visas när man först kommer in på webbsidan.  */
 renderList();
